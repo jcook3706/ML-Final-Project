084c191476ee8e9c38dabb5eff0f8c8a67a71469
jcook3706 <jcook3706@gmail.com>
2023-04-18 21:29:04 -0400

    first working version

---
+++ depth_predictor.py
@@ -0,0 +1,125 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from PIL import Image
+import numpy as np
+import time
+
+class MyNet(nn.Module):
+    def __init__(self):
+        super(MyNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.conv4 = nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        x = nn.functional.relu(self.conv1(x))
+        x = nn.functional.relu(self.conv2(x))
+        x = nn.functional.relu(self.conv3(x))
+        x = nn.functional.sigmoid(self.conv4(x))
+        return x
+
+def convertTGA(num, color):
+    if color:
+        img = Image.open("./dataset/color"+str(num)+".tga")
+    else:
+        img = Image.open("./dataset/depth"+str(num)+".tga")
+    # arr = np.asarray(img)
+    if color:
+        img = img.convert('RGB')
+    else:
+        img = img.convert('L')
+    pixel_data = list(img.getdata())
+    if color:
+        rgb_array = np.array(pixel_data).reshape(img.size[1], img.size[0], 3)
+    else:
+        rgb_array = np.array(pixel_data).reshape(img.size[1], img.size[0], 1)
+    rgb_array = rgb_array/255.0
+    if color:
+        rgb_array = np.reshape(rgb_array, (1, 250, 250, 3))
+    else:
+        rgb_array = np.reshape(rgb_array, (1, 250, 250, 1))
+    return rgb_array
+
+def convertColorImages(numImages=4000, numDivisions=40):
+    colorDataset = None
+    for i in range(numDivisions):
+        print(i)
+        colorImages = None
+        for k in range(int(numImages/numDivisions)):
+            imgData = convertTGA(i*numDivisions+k, True)
+            if colorImages is not None:
+                colorImages = np.concatenate((colorImages, imgData), axis=0)
+            else:
+                colorImages = imgData
+        if colorDataset is not None:
+            colorDataset = np.concatenate((colorDataset, colorImages), axis = 0)
+        else:
+            colorDataset = colorImages
+    return colorDataset
+
+def convertDepthImages(numImages=4000, numDivisions=40):
+    depthDataset = None
+    for i in range(numDivisions):
+        print(i)
+        depthImages = None
+        for k in range(int(numImages/numDivisions)):
+            imgData = convertTGA(i*numDivisions+k, False)
+            if depthImages is not None:
+                depthImages = np.concatenate((depthImages, imgData), axis=0)
+            else:
+                depthImages = imgData
+        if depthDataset is not None:
+            depthDataset = np.concatenate((depthDataset, depthImages), axis = 0)
+        else:
+            depthDataset = depthImages
+    return depthDataset
+
+batchSize = 4
+datasetSize = 400
+
+print('Converting color dataset...')
+colorDataset = convertColorImages(datasetSize, 10)
+print('Color dataset shape: ', colorDataset.shape)
+
+print('Converting depth dataset...')
+depthDataset = convertDepthImages(datasetSize, 10)
+print('Depth dataset shape: ', depthDataset.shape)
+
+# create an instance of the network and pass some data through it
+model = MyNet()
+criterion = nn.MSELoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+inputTensor = torch.from_numpy(colorDataset.transpose((0, 3, 1, 2))).float()
+targetTensor = torch.from_numpy(depthDataset.transpose((0, 3, 1, 2))).float()
+dataset = torch.utils.data.TensorDataset(inputTensor, targetTensor)
+loader = torch.utils.data.DataLoader(dataset, batch_size=batchSize, shuffle=True)
+a = np.squeeze(colorDataset[1])
+a = a*255.0
+img = Image.fromarray(np.uint8(a))
+img.show()
+a = np.squeeze(depthDataset[1])
+a = a*255.0
+img = Image.fromarray(np.uint8(a))
+img.show()
+for epoch in range(10):
+    res = model(torch.from_numpy(colorDataset[1:2].transpose((0, 3, 1, 2))).float())
+    a = np.squeeze(res.detach().numpy())
+    a = a*255.0
+    img = Image.fromarray(np.uint8(a))
+    img.show()
+    print('Epoch: ', epoch)
+    running_loss = 0.0
+    for i, data in enumerate(loader, 0):
+        inputs, targets = data
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 100))
+            running_loss = 0.0
